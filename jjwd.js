59901789fb61f66d0b3e5a19c669ae681528a575
ryo-a <maki62rjtt@gmail.com>
2017-12-11 00:53:50 +0900

    gzip json files

--- jjwd.js
+++ jjwd.js
@@ -22,51 +22,56 @@ const snowDepthReplaceRule = require('./settings/snowDepthReplaceRule.json');
 const fallingSnow24hReplaceRule = require('./settings/fallingSnow24hReplaceRule.json');
 const fallingSnowTotalReplaceRule = require('./settings/fallingSnowTotalReplaceRule.json');
 
-
 /*------------- fetch and save file -------------*/
 /* every 10 minutes */
 cron.schedule('45 */10 * * * *', function(){
-  getCSVandSaveJSON(jmaURL.rain1hURL, rainReplaceRule1h, 'amedas-rain-1h-recent.json');
-  getCSVandSaveJSON(jmaURL.rain3hURL, rainReplaceRule3h, 'amedas-rain-3h-recent.json');
-  getCSVandSaveJSON(jmaURL.rain24hURL, rainReplaceRule24h, 'amedas-rain-24h-recent.json');
-  getCSVandSaveJSON(jmaURL.rain48hURL, rainReplaceRule48h, 'amedas-rain-48h-recent.json');
-  getCSVandSaveJSON(jmaURL.rain72hURL, rainReplaceRule72h, 'amedas-rain-72h-recent.json');
-  getCSVandSaveJSON(jmaURL.rainAllURL, rainReplaceRule, 'amedas-rain-all-recent.json');
+  getCSVandSaveJSON(jmaURL.rain1hURL, rainReplaceRule1h, 'amedas-rain-1h-recent.json', true);
+  getCSVandSaveJSON(jmaURL.rain3hURL, rainReplaceRule3h, 'amedas-rain-3h-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.rain24hURL, rainReplaceRule24h, 'amedas-rain-24h-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.rain48hURL, rainReplaceRule48h, 'amedas-rain-48h-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.rain72hURL, rainReplaceRule72h, 'amedas-rain-72h-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.rainAllURL, rainReplaceRule, 'amedas-rain-all-recent.json' ,true);
 });
 
 /* every 4:00 JST */
 cron.schedule('45 0 04 * * *', function(){
-  getCSVandSaveJSON(jmaURL.fallingSnowTotalURL, fallingSnowTotalReplaceRule, 'amedas-falling-snow-total-recent.json');
+  getCSVandSaveJSON(jmaURL.fallingSnowTotalURL, fallingSnowTotalReplaceRule, 'amedas-falling-snow-total-recent.json' ,true);
 });
 
 /* every *:50:45 */
 cron.schedule('45 50 */1 * * *', function(){
-  getCSVandSaveJSON(jmaURL.maxWindURL, maxWindReplaceRule, 'amedas-max-wind-speed-recent.json');
-  getCSVandSaveJSON(jmaURL.maxIntWindURL, maxIntWindReplaceRule, 'amedas-max-instantaneous-wind-speed-recent.json');
+  getCSVandSaveJSON(jmaURL.maxWindURL, maxWindReplaceRule, 'amedas-max-wind-speed-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.maxIntWindURL, maxIntWindReplaceRule, 'amedas-max-instantaneous-wind-speed-recent.json' ,true);
   
-  getCSVandSaveJSON(jmaURL.maxTempURL, maxTempReplaceRule, 'amedas-max-temperature-recent.json');
-  getCSVandSaveJSON(jmaURL.minTempURL, minTempReplaceRule, 'amedas-min-temperature-recent.json');
+  getCSVandSaveJSON(jmaURL.maxTempURL, maxTempReplaceRule, 'amedas-max-temperature-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.minTempURL, minTempReplaceRule, 'amedas-min-temperature-recent.json' ,true);
   
-  getCSVandSaveJSON(jmaURL.snowDepthURL, snowDepthReplaceRule, 'amedas-snow-depth-recent.json');
-  getCSVandSaveJSON(jmaURL.fallingSnow24hURL, fallingSnow24hReplaceRule, 'amedas-falling-snow-24h-recent.json');
+  getCSVandSaveJSON(jmaURL.snowDepthURL, snowDepthReplaceRule, 'amedas-snow-depth-recent.json' ,true);
+  getCSVandSaveJSON(jmaURL.fallingSnow24hURL, fallingSnow24hReplaceRule, 'amedas-falling-snow-24h-recent.json' ,true);
 });
 
+  
 
 
 
-function getCSVandSaveJSON(target, rule, filename, gzip,gzipfilename) {
+
+function getCSVandSaveJSON(target, rule, fileName, gzip, gzipFileName) {
 
   if (gzip == undefined) {
     gzip = false;
   }
 
+  if (gzipFileName == undefined){
+    gzipFileName = fileName + '.gz';
+  }
+
   client.fetch(target, 'sjis', function (err, $, res, body) {
     let input = csvHeaderReplacerWithJSON($.html(), rule);
     let result = parse(input, { columns: true, relax_column_count: true });
-    fs.writeFileSync(environment.outputPath + '/' + filename, JSON.stringify(result), 'utf8');
+    fs.writeFileSync(environment.outputPath + '/' + fileName, JSON.stringify(result), 'utf8');
     if (gzip == true) {
       zlib.gzip(JSON.stringify(result), function (err, binary) {
-        fs.writeFileSync(environment.outputPath + gzipfilename, binary);
+        fs.writeFileSync(environment.outputPath + '/' + gzipFileName, binary);
       });
     }
   });
